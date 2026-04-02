b1d5f8ccefa799cff55a0b63bd93eff8796c8443
joao_m_lessa <joao_m_lessa@estudante.sc.senai.br>
2026-04-02 20:21:23 -0300

    update title in App component from signal to static string

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('exemplo-um');
+  protected readonly title = 'Angular SCSS';
 }
