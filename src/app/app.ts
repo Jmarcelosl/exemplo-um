a8e60c523831799ccb099a3d1db8892abdd99528
joao_m_lessa <joao_m_lessa@estudante.sc.senai.br>
2026-04-02 21:57:49 -0300

    refactor: restructure app component and add Titulo component with dynamic greeting

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Titulo } from './titulo/titulo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Titulo],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/titulo/titulo.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-titulo',
+  imports: [],
+  templateUrl: './titulo.html',
+  styleUrl: './titulo.scss',
+})
+export class Titulo implements OnInit{
+
+  public mensagem: String = "Boas Vindas";
+
+  ngOnInit(): void {
+    const hora = new Date().getHours();
+    if(hora < 12){
+      this.mensagem = "Bom dia!";
+    }else if(hora > 18){
+      this.mensagem = "Boa noite!"
+    }else{
+      this.mensagem = "Boa tarde!";
+    }
+  }
+
+}
